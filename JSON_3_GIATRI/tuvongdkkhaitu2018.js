07f53d25e436cc69291ac7c78d21a6c157530272
Lê Ngọc Trâm Anh <tramanh84265@gmail.com>
2020-09-01 10:28:50 +0700

    bullet

--- JSON_3_GIATRI/tuvongdkkhaitu2018.js
+++ JSON_3_GIATRI/tuvongdkkhaitu2018.js
@@ -1,380 +1,380 @@
 [
-    {
-        "Tên tỉnh": "Hà Nội",
-        "Tổng số": 37.014,
-        "Đúng hạn": 31.707,
-        "Quá hạn": 5.307
-    },
-    {
-        "Tên tỉnh": "Vĩnh Phúc",
-        "Tổng số": 5.525,
-        "Đúng hạn": 5.057,
-        "Quá hạn": 468
-    },
-    {
-        "Tên tỉnh": "Bắc Ninh",
-        "Tổng số": 6.346,
-        "Đúng hạn": 5.54,
-        "Quá hạn": 806
-    },
-    {
-        "Tên tỉnh": "Quảng Ninh",
-        "Tổng số": 7.145,
-        "Đúng hạn": 5.477,
-        "Quá hạn": 1.668
-    },
-    {
-        "Tên tỉnh": "Hải Dương",
-        "Tổng số": 10.857,
-        "Đúng hạn": 10.266,
-        "Quá hạn": 591
-    },
-    {
-        "Tên tỉnh": "Hải Phòng",
-        "Tổng số": 12.401,
-        "Đúng hạn": 11.057,
-        "Quá hạn": 1.344
-    },
-    {
-        "Tên tỉnh": "Hưng Yên",
-        "Tổng số": 7.749,
-        "Đúng hạn": 6.589,
-        "Quá hạn": 1.16
-    },
-    {
-        "Tên tỉnh": "Thái Bình",
-        "Tổng số": 12.056,
-        "Đúng hạn": 11.259,
-        "Quá hạn": 797
-    },
-    {
-        "Tên tỉnh": "Hà Nam",
-        "Tổng số": 5.944,
-        "Đúng hạn": 4.81,
-        "Quá hạn": 1.134
-    },
-    {
-        "Tên tỉnh": "Nam Định",
-        "Tổng số": 9.55,
-        "Đúng hạn": 7.744,
-        "Quá hạn": 1.806
-    },
-    {
-        "Tên tỉnh": "Ninh Bình",
-        "Tổng số": 5.597,
-        "Đúng hạn": 4.581,
-        "Quá hạn": 1.016
-    },
-    {
-        "Tên tỉnh": "Hà Giang",
-        "Tổng số": 4.196,
-        "Đúng hạn": 2.597,
-        "Quá hạn": 1.599
-    },
-    {
-        "Tên tỉnh": "Cao Bằng",
-        "Tổng số": 3.594,
-        "Đúng hạn": 1.879,
-        "Quá hạn": 1.715
-    },
-    {
-        "Tên tỉnh": "Bắc Kạn",
-        "Tổng số": 1.84,
-        "Đúng hạn": 1.446,
-        "Quá hạn": 394
-    },
-    {
-        "Tên tỉnh": "Tuyên Quang",
-        "Tổng số": 4.791,
-        "Đúng hạn": 3.614,
-        "Quá hạn": 1.177
-    },
-    {
-        "Tên tỉnh": "Lào Cai",
-        "Tổng số": 3.274,
-        "Đúng hạn": 1.901,
-        "Quá hạn": 1.373
-    },
-    {
-        "Tên tỉnh": "Yên Bái",
-        "Tổng số": 4.44,
-        "Đúng hạn": 3.448,
-        "Quá hạn": 992
-    },
-    {
-        "Tên tỉnh": "Thái Nguyên",
-        "Tổng số": 7.255,
-        "Đúng hạn": 5.662,
-        "Quá hạn": 1.593
-    },
-    {
-        "Tên tỉnh": "Lạng Sơn",
-        "Tổng số": 6.061,
-        "Đúng hạn": 3.287,
-        "Quá hạn": 2.774
-    },
-    {
-        "Tên tỉnh": "Bắc Giang",
-        "Tổng số": 8.624,
-        "Đúng hạn": 7.052,
-        "Quá hạn": 1.572
-    },
-    {
-        "Tên tỉnh": "Phú Thọ",
-        "Tổng số": 8.04,
-        "Đúng hạn": 7.23,
-        "Quá hạn": 810
-    },
-    {
-        "Tên tỉnh": "Điện Biên",
-        "Tổng số": 2.59,
-        "Đúng hạn": 1.499,
-        "Quá hạn": 1.091
-    },
-    {
-        "Tên tỉnh": "Lai Châu",
-        "Tổng số": 1.769,
-        "Đúng hạn": 853,
-        "Quá hạn": 916
-    },
-    {
-        "Tên tỉnh": "Sơn La",
-        "Tổng số": 6.288,
-        "Đúng hạn": 3.541,
-        "Quá hạn": 2.747
-    },
-    {
-        "Tên tỉnh": "Hòa Bình",
-        "Tổng số": 4.812,
-        "Đúng hạn": 4.068,
-        "Quá hạn": 744
-    },
-    {
-        "Tên tỉnh": "Thanh Hóa",
-        "Tổng số": 25.053,
-        "Đúng hạn": 17.011,
-        "Quá hạn": 8.042
-    },
-    {
-        "Tên tỉnh": "Nghệ An",
-        "Tổng số": 18.912,
-        "Đúng hạn": 11.289,
-        "Quá hạn": 7.623
-    },
-    {
-        "Tên tỉnh": "Hà Tĩnh",
-        "Tổng số": 8.648,
-        "Đúng hạn": 5.109,
-        "Quá hạn": 3.539
-    },
-    {
-        "Tên tỉnh": "Quảng Bình",
-        "Tổng số": 6.089,
-        "Đúng hạn": 3.931,
-        "Quá hạn": 2.158
-    },
-    {
-        "Tên tỉnh": "Quảng Trị",
-        "Tổng số": 3.728,
-        "Đúng hạn": 2.568,
-        "Quá hạn": 1.16
-    },
-    {
-        "Tên tỉnh": "Thừa Thiên - Huế",
-        "Tổng số": 9.816,
-        "Đúng hạn": 4.25,
-        "Quá hạn": 5.566
-    },
-    {
-        "Tên tỉnh": "Đà Nẵng",
-        "Tổng số": 6.741,
-        "Đúng hạn": 4.13,
-        "Quá hạn": 2.611
-    },
-    {
-        "Tên tỉnh": "Quảng Nam",
-        "Tổng số": 13.03,
-        "Đúng hạn": 5.93,
-        "Quá hạn": 7.1
-    },
-    {
-        "Tên tỉnh": "Quảng Ngãi",
-        "Tổng số": 12.236,
-        "Đúng hạn": 5.147,
-        "Quá hạn": 7.089
-    },
-    {
-        "Tên tỉnh": "Bình Định",
-        "Tổng số": 10.795,
-        "Đúng hạn": 6.617,
-        "Quá hạn": 4.178
-    },
-    {
-        "Tên tỉnh": "Phú Yên",
-        "Tổng số": 5.891,
-        "Đúng hạn": 4.252,
-        "Quá hạn": 1.639
-    },
-    {
-        "Tên tỉnh": "Khánh Hòa",
-        "Tổng số": 7.875,
-        "Đúng hạn": 5.315,
-        "Quá hạn": 2.56
-    },
-    {
-        "Tên tỉnh": "Ninh Thuận",
-        "Tổng số": 4.193,
-        "Đúng hạn": 2.505,
-        "Quá hạn": 1.688
-    },
-    {
-        "Tên tỉnh": "Bình Thuận",
-        "Tổng số": 10.581,
-        "Đúng hạn": 5.865,
-        "Quá hạn": 4.716
-    },
-    {
-        "Tên tỉnh": "Kon Tum",
-        "Tổng số": 2.789,
-        "Đúng hạn": 1.23,
-        "Quá hạn": 1.559
-    },
-    {
-        "Tên tỉnh": "Gia Lai",
-        "Tổng số": 8.724,
-        "Đúng hạn": 3.758,
-        "Quá hạn": 4.966
-    },
-    {
-        "Tên tỉnh": "Đắk Lắk",
-        "Tổng số": 9.643,
-        "Đúng hạn": 5.376,
-        "Quá hạn": 4.267
-    },
-    {
-        "Tên tỉnh": "Đắk Nông",
-        "Tổng số": 2.538,
-        "Đúng hạn": 1.33,
-        "Quá hạn": 1.208
-    },
-    {
-        "Tên tỉnh": "Lâm Đồng",
-        "Tổng số": 5.968,
-        "Đúng hạn": 4.037,
-        "Quá hạn": 1.931
-    },
-    {
-        "Tên tỉnh": "Bình Phước",
-        "Tổng số": 6.454,
-        "Đúng hạn": 4.988,
-        "Quá hạn": 1.466
-    },
-    {
-        "Tên tỉnh": "Tây Ninh",
-        "Tổng số": 8.115,
-        "Đúng hạn": 6.473,
-        "Quá hạn": 1.642
-    },
-    {
-        "Tên tỉnh": "Bình Dương",
-        "Tổng số": 6.922,
-        "Đúng hạn": 4.907,
-        "Quá hạn": 2.015
-    },
-    {
-        "Tên tỉnh": "Đồng Nai",
-        "Tổng số": 12.966,
-        "Đúng hạn": 11.352,
-        "Quá hạn": 1.614
-    },
-    {
-        "Tên tỉnh": "Bà Rịa - Vũng Tàu",
-        "Tổng số": 5.768,
-        "Đúng hạn": 4.888,
-        "Quá hạn": 880
-    },
-    {
-        "Tên tỉnh": "TP. Hồ Chí Minh",
-        "Tổng số": 34.845,
-        "Đúng hạn": 31.086,
-        "Quá hạn": 3.759
-    },
-    {
-        "Tên tỉnh": "Long An",
-        "Tổng số": 11.077,
-        "Đúng hạn": 8.263,
-        "Quá hạn": 2.814
-    },
-    {
-        "Tên tỉnh": "Tiền Giang",
-        "Tổng số": 12.325,
-        "Đúng hạn": 10.173,
-        "Quá hạn": 2.152
-    },
-    {
-        "Tên tỉnh": "Bến Tre",
-        "Tổng số": 10.355,
-        "Đúng hạn": 7.85,
-        "Quá hạn": 2.505
-    },
-    {
-        "Tên tỉnh": "Trà Vinh",
-        "Tổng số": 7.421,
-        "Đúng hạn": 4.713,
-        "Quá hạn": 2.708
-    },
-    {
-        "Tên tỉnh": "Vĩnh Long",
-        "Tổng số": 7.515,
-        "Đúng hạn": 5.651,
-        "Quá hạn": 1.864
-    },
-    {
-        "Tên tỉnh": "Đồng Tháp",
-        "Tổng số": 13.089,
-        "Đúng hạn": 8.664,
-        "Quá hạn": 4.425
-    },
-    {
-        "Tên tỉnh": "An Giang",
-        "Tổng số": 20.704,
-        "Đúng hạn": 12.904,
-        "Quá hạn": 7.8
-    },
-    {
-        "Tên tỉnh": "Kiên Giang",
-        "Tổng số": 12.384,
-        "Đúng hạn": 6.954,
-        "Quá hạn": 5.43
-    },
-    {
-        "Tên tỉnh": "Cần Thơ",
-        "Tổng số": 8.422,
-        "Đúng hạn": 5.733,
-        "Quá hạn": 2.689
-    },
-    {
-        "Tên tỉnh": "Hậu Giang",
-        "Tổng số": 3.888,
-        "Đúng hạn": 2.778,
-        "Quá hạn": 1.11
-    },
-    {
-        "Tên tỉnh": "Sóc Trăng",
-        "Tổng số": 12.746,
-        "Đúng hạn": 6.129,
-        "Quá hạn": 6.617
-    },
-    {
-        "Tên tỉnh": "Bạc Liêu",
-        "Tổng số": 6.681,
-        "Đúng hạn": 3.494,
-        "Quá hạn": 3.187
-    },
-    {
-        "Tên tỉnh": "Cà Mau",
-        "Tổng số": 6.653,
-        "Đúng hạn": 4.547,
-        "Quá hạn": 2.106
-    }
+  {
+    "Tên tỉnh": "Hà Nội",
+    "Tổng số": 37014,
+    "Đúng hạn": 31707,
+    "Quá hạn": 5307
+  },
+  {
+    "Tên tỉnh": "Vĩnh Phúc",
+    "Tổng số": 5525,
+    "Đúng hạn": 5057,
+    "Quá hạn": 468
+  },
+  {
+    "Tên tỉnh": "Bắc Ninh",
+    "Tổng số": 6346,
+    "Đúng hạn": 5540,
+    "Quá hạn": 806
+  },
+  {
+    "Tên tỉnh": "Quảng Ninh",
+    "Tổng số": 7145,
+    "Đúng hạn": 5477,
+    "Quá hạn": 1668
+  },
+  {
+    "Tên tỉnh": "Hải Dương",
+    "Tổng số": 10857,
+    "Đúng hạn": 10266,
+    "Quá hạn": 591
+  },
+  {
+    "Tên tỉnh": "Hải Phòng",
+    "Tổng số": 12401,
+    "Đúng hạn": 11057,
+    "Quá hạn": 1344
+  },
+  {
+    "Tên tỉnh": "Hưng Yên",
+    "Tổng số": 7749,
+    "Đúng hạn": 6589,
+    "Quá hạn": 1160
+  },
+  {
+    "Tên tỉnh": "Thái Bình",
+    "Tổng số": 12056,
+    "Đúng hạn": 11259,
+    "Quá hạn": 797
+  },
+  {
+    "Tên tỉnh": "Hà Nam",
+    "Tổng số": 5944,
+    "Đúng hạn": 4810,
+    "Quá hạn": 1134
+  },
+  {
+    "Tên tỉnh": "Nam Định",
+    "Tổng số": 9550,
+    "Đúng hạn": 7744,
+    "Quá hạn": 1806
+  },
+  {
+    "Tên tỉnh": "Ninh Bình",
+    "Tổng số": 5597,
+    "Đúng hạn": 4581,
+    "Quá hạn": 1016
+  },
+  {
+    "Tên tỉnh": "Hà Giang",
+    "Tổng số": 4196,
+    "Đúng hạn": 2597,
+    "Quá hạn": 1599
+  },
+  {
+    "Tên tỉnh": "Cao Bằng",
+    "Tổng số": 3594,
+    "Đúng hạn": 1879,
+    "Quá hạn": 1715
+  },
+  {
+    "Tên tỉnh": "Bắc Kạn",
+    "Tổng số": 1840,
+    "Đúng hạn": 1446,
+    "Quá hạn": 394
+  },
+  {
+    "Tên tỉnh": "Tuyên Quang",
+    "Tổng số": 4791,
+    "Đúng hạn": 3614,
+    "Quá hạn": 1177
+  },
+  {
+    "Tên tỉnh": "Lào Cai",
+    "Tổng số": 3274,
+    "Đúng hạn": 1901,
+    "Quá hạn": 1373
+  },
+  {
+    "Tên tỉnh": "Yên Bái",
+    "Tổng số": 4440,
+    "Đúng hạn": 3448,
+    "Quá hạn": 992
+  },
+  {
+    "Tên tỉnh": "Thái Nguyên",
+    "Tổng số": 7255,
+    "Đúng hạn": 5662,
+    "Quá hạn": 1593
+  },
+  {
+    "Tên tỉnh": "Lạng Sơn",
+    "Tổng số": 6061,
+    "Đúng hạn": 3287,
+    "Quá hạn": 2774
+  },
+  {
+    "Tên tỉnh": "Bắc Giang",
+    "Tổng số": 8624,
+    "Đúng hạn": 7052,
+    "Quá hạn": 1572
+  },
+  {
+    "Tên tỉnh": "Phú Thọ",
+    "Tổng số": 8040,
+    "Đúng hạn": 7230,
+    "Quá hạn": 810
+  },
+  {
+    "Tên tỉnh": "Điện Biên",
+    "Tổng số": 2590,
+    "Đúng hạn": 1499,
+    "Quá hạn": 1091
+  },
+  {
+    "Tên tỉnh": "Lai Châu",
+    "Tổng số": 1769,
+    "Đúng hạn": 853,
+    "Quá hạn": 916
+  },
+  {
+    "Tên tỉnh": "Sơn La",
+    "Tổng số": 6288,
+    "Đúng hạn": 3541,
+    "Quá hạn": 2747
+  },
+  {
+    "Tên tỉnh": "Hòa Bình",
+    "Tổng số": 4812,
+    "Đúng hạn": 4068,
+    "Quá hạn": 744
+  },
+  {
+    "Tên tỉnh": "Thanh Hóa",
+    "Tổng số": 25053,
+    "Đúng hạn": 17011,
+    "Quá hạn": 8042
+  },
+  {
+    "Tên tỉnh": "Nghệ An",
+    "Tổng số": 18912,
+    "Đúng hạn": 11289,
+    "Quá hạn": 7623
+  },
+  {
+    "Tên tỉnh": "Hà Tĩnh",
+    "Tổng số": 8648,
+    "Đúng hạn": 5109,
+    "Quá hạn": 3539
+  },
+  {
+    "Tên tỉnh": "Quảng Bình",
+    "Tổng số": 6089,
+    "Đúng hạn": 3931,
+    "Quá hạn": 2158
+  },
+  {
+    "Tên tỉnh": "Quảng Trị",
+    "Tổng số": 3728,
+    "Đúng hạn": 2568,
+    "Quá hạn": 1160
+  },
+  {
+    "Tên tỉnh": "Thừa Thiên - Huế",
+    "Tổng số": 9816,
+    "Đúng hạn": 4250,
+    "Quá hạn": 5566
+  },
+  {
+    "Tên tỉnh": "Đà Nẵng",
+    "Tổng số": 6741,
+    "Đúng hạn": 4130,
+    "Quá hạn": 2611
+  },
+  {
+    "Tên tỉnh": "Quảng Nam",
+    "Tổng số": 13030,
+    "Đúng hạn": 5930,
+    "Quá hạn": 7100
+  },
+  {
+    "Tên tỉnh": "Quảng Ngãi",
+    "Tổng số": 12236,
+    "Đúng hạn": 5147,
+    "Quá hạn": 7089
+  },
+  {
+    "Tên tỉnh": "Bình Định",
+    "Tổng số": 10795,
+    "Đúng hạn": 6617,
+    "Quá hạn": 4178
+  },
+  {
+    "Tên tỉnh": "Phú Yên",
+    "Tổng số": 5891,
+    "Đúng hạn": 4252,
+    "Quá hạn": 1639
+  },
+  {
+    "Tên tỉnh": "Khánh Hòa",
+    "Tổng số": 7875,
+    "Đúng hạn": 5315,
+    "Quá hạn": 2560
+  },
+  {
+    "Tên tỉnh": "Ninh Thuận",
+    "Tổng số": 4193,
+    "Đúng hạn": 2505,
+    "Quá hạn": 1688
+  },
+  {
+    "Tên tỉnh": "Bình Thuận",
+    "Tổng số": 10581,
+    "Đúng hạn": 5865,
+    "Quá hạn": 4716
+  },
+  {
+    "Tên tỉnh": "Kon Tum",
+    "Tổng số": 2789,
+    "Đúng hạn": 1230,
+    "Quá hạn": 1559
+  },
+  {
+    "Tên tỉnh": "Gia Lai",
+    "Tổng số": 8724,
+    "Đúng hạn": 3758,
+    "Quá hạn": 4966
+  },
+  {
+    "Tên tỉnh": "Đắk Lắk",
+    "Tổng số": 9643,
+    "Đúng hạn": 5376,
+    "Quá hạn": 4267
+  },
+  {
+    "Tên tỉnh": "Đắk Nông",
+    "Tổng số": 2538,
+    "Đúng hạn": 1330,
+    "Quá hạn": 1208
+  },
+  {
+    "Tên tỉnh": "Lâm Đồng",
+    "Tổng số": 5968,
+    "Đúng hạn": 4037,
+    "Quá hạn": 1931
+  },
+  {
+    "Tên tỉnh": "Bình Phước",
+    "Tổng số": 6454,
+    "Đúng hạn": 4988,
+    "Quá hạn": 1466
+  },
+  {
+    "Tên tỉnh": "Tây Ninh",
+    "Tổng số": 8115,
+    "Đúng hạn": 6473,
+    "Quá hạn": 1642
+  },
+  {
+    "Tên tỉnh": "Bình Dương",
+    "Tổng số": 6922,
+    "Đúng hạn": 4907,
+    "Quá hạn": 2015
+  },
+  {
+    "Tên tỉnh": "Đồng Nai",
+    "Tổng số": 12966,
+    "Đúng hạn": 11352,
+    "Quá hạn": 1614
+  },
+  {
+    "Tên tỉnh": "Bà Rịa - Vũng Tàu",
+    "Tổng số": 5768,
+    "Đúng hạn": 4888,
+    "Quá hạn": 880
+  },
+  {
+    "Tên tỉnh": "TP. Hồ Chí Minh",
+    "Tổng số": 34845,
+    "Đúng hạn": 31086,
+    "Quá hạn": 3759
+  },
+  {
+    "Tên tỉnh": "Long An",
+    "Tổng số": 11077,
+    "Đúng hạn": 8263,
+    "Quá hạn": 2814
+  },
+  {
+    "Tên tỉnh": "Tiền Giang",
+    "Tổng số": 12325,
+    "Đúng hạn": 10173,
+    "Quá hạn": 2152
+  },
+  {
+    "Tên tỉnh": "Bến Tre",
+    "Tổng số": 10355,
+    "Đúng hạn": 7850,
+    "Quá hạn": 2505
+  },
+  {
+    "Tên tỉnh": "Trà Vinh",
+    "Tổng số": 7421,
+    "Đúng hạn": 4713,
+    "Quá hạn": 2708
+  },
+  {
+    "Tên tỉnh": "Vĩnh Long",
+    "Tổng số": 7515,
+    "Đúng hạn": 5651,
+    "Quá hạn": 1864
+  },
+  {
+    "Tên tỉnh": "Đồng Tháp",
+    "Tổng số": 13089,
+    "Đúng hạn": 8664,
+    "Quá hạn": 4425
+  },
+  {
+    "Tên tỉnh": "An Giang",
+    "Tổng số": 20704,
+    "Đúng hạn": 12904,
+    "Quá hạn": 7800
+  },
+  {
+    "Tên tỉnh": "Kiên Giang",
+    "Tổng số": 12384,
+    "Đúng hạn": 6954,
+    "Quá hạn": 5430
+  },
+  {
+    "Tên tỉnh": "Cần Thơ",
+    "Tổng số": 8422,
+    "Đúng hạn": 5733,
+    "Quá hạn": 2689
+  },
+  {
+    "Tên tỉnh": "Hậu Giang",
+    "Tổng số": 3888,
+    "Đúng hạn": 2778,
+    "Quá hạn": 1110
+  },
+  {
+    "Tên tỉnh": "Sóc Trăng",
+    "Tổng số": 12746,
+    "Đúng hạn": 6129,
+    "Quá hạn": 6617
+  },
+  {
+    "Tên tỉnh": "Bạc Liêu",
+    "Tổng số": 6681,
+    "Đúng hạn": 3494,
+    "Quá hạn": 3187
+  },
+  {
+    "Tên tỉnh": "Cà Mau",
+    "Tổng số": 6653,
+    "Đúng hạn": 4547,
+    "Quá hạn": 2106
+  }
 ]
